a339c75fc487ae35adabf6bf90929df5ffdb0dd3
Moon-ki Kim <mkmkqoa@gmail.com>
2021-10-07 03:10:27 +0900

    Update SpeechRecognitionGoogleAPI.py

--- run python in java/SpeechRecognitionGoogleAPI.py
+++ run python in java/SpeechRecognitionGoogleAPI.py
@@ -9,17 +9,17 @@ def launch(triggerWord):
     
     while(True):
         with sr.Microphone() as source:
-            print("Say Something : ")
+            print(0)
             audio = r.listen(source)
             try:
                 text = r.recognize_google(audio, language='ko-KR')
-                print("You Said : {}".format(text))
+                print(0)
             except:
-                print("Sorry could not recognize")
+                print(-1)
 
         if triggerWord in text:
             trigger = True
-            print("triggered")
+            print(1)
             break
 
 def main(argv):
@@ -27,32 +27,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
-
-
-
-"""
-import speech_recognition as sr
-
-r = sr.Recognizer()
-
-text = ''
-triggerWord = '맵피'
-trigger = False
-
-while (True):
-    with sr.Microphone() as source:
-        print("Say Something : ")
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio, language='ko-KR')
-            print("You Said : {}".format(text))
-        except:
-            print("Sorry could not recognize")
-        
-    if triggerWord in text:
-        trigger = True
-        print("triggered")
-        break
-
-print("End loop")
-"""
