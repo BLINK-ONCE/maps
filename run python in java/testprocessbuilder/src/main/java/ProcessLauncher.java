cf0ade9ad07e318eb1f00dc4ebe4056806435483
Moon-ki Kim <mkmkqoa@gmail.com>
2021-10-08 23:24:27 +0900

    Make execPython to repeat and check trigger

--- run python in java/testprocessbuilder/src/main/java/ProcessLauncher.java
+++ run python in java/testprocessbuilder/src/main/java/ProcessLauncher.java
@@ -1,6 +1,7 @@
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,18 +13,23 @@ import sun.font.TrueTypeFont;
 public class ProcessLauncher {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Python call");
+        boolean trigger = false;
         String[] command = new String[2];
         command[0] = "python";
         command[1] = "C:/Users/pc/Desktop/git-repos/maps/run python in java/SpeechRecognitionGoogleAPI.py";
 
-        try {
-            execPython(command);
-        } catch (Exception e) {
-            e.printStackTrace();
+        while (!trigger) {
+            try {
+                trigger = execPython(command);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("trigger: " + trigger);
         }
+
     }
 
-    public static void execPython(String[] command) throws IOException, InterruptedException {
+    public static boolean execPython(String[] command) throws IOException, InterruptedException {
         CommandLine commandLine = CommandLine.parse(command[0]);
         for (int i = 1, n = command.length; i < n; i++) {
             commandLine.addArgument(command[i]);
@@ -35,6 +41,12 @@ public class ProcessLauncher {
         executor.setStreamHandler(pumpStreamHandler);
         int result = executor.execute(commandLine);
         System.out.println("result: " + result);
-        System.out.println("output: " + outputStream.toString());
+
+        String output = outputStream.toString();
+        System.out.println("output: " + output);
+        if ("1".equals(output.substring(0,1))) {
+            return true;
+        }
+        return false;
     }
 }
