0785bd03c3e99958d6f09c470f91ec843691d7b1
Moon-ki Kim <mkmkqoa@gmail.com>
2021-10-08 22:31:13 +0900

    Update test.py

--- run python in java/test.py
+++ run python in java/test.py
@@ -1,13 +1,8 @@
 import sys
 
 def sum(v1,v2):
-    i = 0
-    while(True):
-        result = int(v1) + int(v2)
-        i += 1
-        if (i >= 10) break
-    print(result)
-
+    result = int(v1) + int(v2)
+    print(result)result)
 
 def main(argv):
     sum(argv[1], argv[2])
